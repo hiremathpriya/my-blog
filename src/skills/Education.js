28a80b2e96e3d63a23d9663d36b8b91a1bb469fb
Priya Hiremath <priyahrish18@gmail.com>
2018-06-17 20:41:48 +1200

    code cleaning

--- src/skills/Education.js
+++ src/skills/Education.js
@@ -5,36 +5,7 @@ import './skills.css';
 class Education extends Component {
   render() {
     return (
-//         <div className="hero native-js skills" id="Education">
-//             <div className="hero-body">
-//             <div className="contanier">
-            
-//             <div className="has-text-centered is-half has-text-Black-bold title font">Education </div>
-//                 <div className="level">
-//                     <div className="has-text-black level-item"> 
-                    
-//                             <ul className="font is-size-5"> 
-//                                 <li>Enspiral Dev Academy Web Development Bootcamp</li>
-//                                 <li>ISTQB Certification</li>
-//                                 <li>Diploma in Software Testing</li>
-//                                 <li>Software Testing (Foundation)</li>
-//                                 <li> Diploma in Hotel Management & Catering Technology - 1999</li>
-//                                 <li>BA (Hons) Psychology + Travel & Tourism – 1996</li>
-//                                 <li>Diploma in Home Science – 1993</li>
-//                             </ul>
-                        
-//                     </div>
-//                 </div>
-                            
-//             </div>
-//             </div>
-//       </div>
-     
-//     );
-//   }
-// }
-
-                <div className="hero native-js skills" id="Education">
+         <div className="hero native-js skills" id="Education">
              <div className="hero-body">
              <div className="contanier">
             
