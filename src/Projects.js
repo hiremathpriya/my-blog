aea683ea29b253cd16118286e33e596f9ebb5d35
Priya Hiremath <priyahrish18@gmail.com>
2018-08-02 09:56:05 +1200

    Added current project work to my resume

--- src/Projects.js
+++ src/Projects.js
@@ -10,7 +10,8 @@ class Projects extends Component {
                     <div className="container">
           
                          <h1 className="has-text-Black is-bold title font has-text-centered">Projects</h1>
-                    
+                         <div className="has-text-Black-bold has-text-centered font is-size-5"> Work In Progress: 'Sahil-Project' Charity-Website United Kingdom(Wordpress)</div>
+                    <div className="has-text-Black-bold has-text-centered font is-size-5">Work In Progress: 'Legal-Checkup' Charity-IT Project</div>
                          <div className="has-text-Black-bold has-text-centered font is-size-5"><a href="https://embracing-blue.herokuapp.com/#/"> 'Embracing-blue' Final group Project- Web development bootcamp</a></div>
                          <div className="has-text-Black-bold has-text-centered font is-size-5"><a href="http://lostfoundpets.herokuapp.com/#/"> 'Lost and found' Semi-Final group Project- Web development bootcamp</a></div>
                 <div className="has-text-Black-bold has-text-centered font is-size-5"><a href="https://cooking-mums.herokuapp.com/"> 'Cooking-mums' Personal Project- Web development bootcamp work in progess</a></div>
