a8f641c01e0665d8174b7b6c47ce74ebe0ebbfe6
Priya Hiremath <priyahrish18@gmail.com>
2019-10-25 22:35:09 +1300

    updating blog

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -11,7 +11,9 @@ class AboutMe extends Component {
               <img className="image is-128x128" src="../../images/DSC_0317.jpg" alt="authorImage" />
             </div>
               <h1 className="has-text-Black is-bold title font has-text-centered">Nice to meet you,</h1>
-                <h2 className="has-text-Black-bold has-text-left font is-size-5"> Hi! I am Priya Hiremath, I am a web developer/Tester who is on this new tech journey.
+                <h2 className="has-text-Black-bold has-text-left font is-size-5"> Hi! I am Priya, I am a web developer/Tester  and this is my journey.
+                 I will be writing about all the life experience and how it has been moulded so far in my 44 years of life.
+                 It is been facinating, Intresting and Inspiring. I will start with one chapter of my life at a time and see how life takes place.
                 I am looking for a fresh new start to my career in software testing field and at the same time challenge myself to do something completely new.
                   I love meeting people and learning about their stories through respect and empathy. I also love to share my experience as an Individual, a Mum and as a Chef.
                   My inspiration to get into the tech world are my kids. In this 'DIGI' world everything revolves around the technology and I want to be a part of that.  
